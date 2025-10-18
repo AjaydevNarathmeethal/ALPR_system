cadf14b2a6f9450af346ecd617f598f46f20ea22
Ajaydev Narathmeethal <ajaydev.narathmeethal@gmail.com>
2025-10-18 00:40:08 -0400

    Initial commit

---
+++ ocr_license_plate_video.py
@@ -0,0 +1,44 @@
+import cv2
+from anpr import PyImageSearchANPR
+
+def cleanup_text(text):
+	# strip out non-ASCII text so we can draw the text on the image
+	# using OpenCV
+	return "".join([c if ord(c) < 128 else "" for c in text]).strip()
+
+cap = cv2.VideoCapture('a.mp4')
+anpr = PyImageSearchANPR(minAR=3.5,maxAR=6)
+
+while (cap.isOpened()):
+    ret,img = cap.read()
+
+    if ret == True:
+        # cv2.imshow('Orginal Video', img)
+
+        if(cv2.waitKey(25) & 0xFF == ord('q')):
+            break
+        (lpText, lpCnt) = anpr.find_and_ocr(img)
+	    
+        if lpText is not None and lpCnt is not None:
+            box = cv2.boxPoints(cv2.minAreaRect(lpCnt))
+            box = box.astype("int")
+            cv2.drawContours(img, [box], -1, (0, 255, 0), 2)
+
+            (x, y, w, h) = cv2.boundingRect(lpCnt)
+            cv2.putText(img, cleanup_text(lpText), (x, y - 15),
+                cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0, 255, 0), 2)
+            # show the output ANPR image
+            #print("[INFO] {}".format(lpText))
+            # cv2.imshow("Output ANPR", img)
+            # cv2.waitKey(0)
+            # cv2.destroyWindow('Output ANPR')
+
+        # show the license number in the original video    
+        cv2.imshow('Video', img)
+    
+    else:
+        break
+
+
+cap.release()
+cv2.destroyAllWindows()
